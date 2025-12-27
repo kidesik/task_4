65dfa4af80c60d2894bb7a10ea924254d1628ec9
kidesik <elizafox2005@gmail.com>
2025-12-27 11:36:00 +0300

    Add files via upload

---
+++ task_4_12/task_4_12/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Hello, World!");
+int r = Convert.ToInt32(Console.ReadLine());
+int a = Convert.ToInt32(Console.ReadLine());
+double s_s = Math.PI * r;
+double S_a = Math.Pow(a, 2);
+if (s_s > S_a)
+{
+    Console.WriteLine("Площадь круга больше");
+}
+else if (s_s < S_a)
+{
+    Console.WriteLine("площадь квадрата больше");
+}
+else
+{
+    Console.WriteLine("площади равны");
+}
