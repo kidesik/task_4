f33120ca9dd74327c854abd393df09eda83b126e
kidesik <elizafox2005@gmail.com>
2025-12-28 21:50:54 +0300

    Add files via upload

--- task_4_12/task_4_12/Program.cs
+++ task_4_12/task_4_12/Program.cs
@@ -1,17 +1,18 @@
 ﻿Console.WriteLine("Hello, World!");
+//Площадь круга: \(S_{ круга}=\pi \cdot r^{2}\) (где \(r\) – радиус).Площадь квадрата: \(S_{ квадрата}= a ^{ 2}\) (где \(a\) – сторона)
 int r = Convert.ToInt32(Console.ReadLine());
-int a = Convert.ToInt32(Console.ReadLine());
-double s_s = Math.PI * r;
-double S_a = Math.Pow(a, 2);
-if (s_s > S_a)
+int a  = Convert.ToInt32(Console.ReadLine());
+double s_circle = r * Math.PI;
+double s_square = Math.Pow(a, 2);
+if (s_square < s_circle)
 {
-    Console.WriteLine("Площадь круга больше");
+    Console.WriteLine($"Площадь круга {s_circle} больше, чем площадь квадрата");
 }
-else if (s_s < S_a)
+else if (s_square > s_circle)
 {
-    Console.WriteLine("площадь квадрата больше");
+    Console.WriteLine($"Площадь квадрата {s_square} больше, чем площадь круга");
 }
 else
 {
-    Console.WriteLine("площади равны");
+    Console.WriteLine("Площади равны");
 }
---
+++ task_4_13/task_4_13/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите массу и объем первого тела и затем введите массу и объем второго тела");
+int m_1 = Convert.ToInt32(Console.ReadLine());
+int v_1  = Convert.ToInt32(Console.ReadLine());
+int m_2  = Convert.ToInt32(Console.ReadLine());
+int v_2  = Convert.ToInt32(Console.ReadLine());
+int p_1 = m_1 / v_1;
+int p_2 = m_2 / v_2;
+if  (p_1 < p_2)
+{
+    Console.WriteLine($"Плотность второго тела больше плотности первого и равно {p_2}");
+}
+else if (p_2 < p_1)
+{
+    Console.WriteLine($"Плотность первого тела больше плотности второго и равно {p_1}");
+}
+else
+{
+    Console.WriteLine("обе плотности равны");
+}
